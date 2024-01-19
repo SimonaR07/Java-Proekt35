4e3bf17411d75a46e8e7c1d0aed8f0d4677e514a
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 21:02:10 +0100

    Add new java program

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+
+
+public class MyClass {
+	public static void main (String []args) {
+		Vehicle v1 = new Vehicle("RED");
+		System.out.println(v1);
+		
+		Vehicle v2 = new Vehicle();
+		System.out.println(v2);
+		
+	}
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,17 @@
+
+public class Vehicle {
+	private String color;
+
+	Vehicle(String a) {
+		color=a;
+
+	}
+	Vehicle(){
+		color="BLUE";
+	}
+
+	public String toString() {
+		return color;
+	}
+
+}
